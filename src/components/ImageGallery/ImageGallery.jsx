8b822c0c515466ca2c1d0fcf3f5c9e9c6e29b4bf
Anna Ojdana <anna.ojdana@gmail.com>
2022-09-05 19:24:17 +0200

    ImageGallery - code refactor

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,30 +1,28 @@
 import styles from './ImageGallery.module.css';
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 import { nanoid } from 'nanoid';
 
-class ImageGallery extends Component {
-  render() {
-    const { gallery } = styles;
-    const { imagesData, openModal } = this.props;
-    return (
-      <ul className={gallery}>
-        {imagesData.map(imageData => (
-          <ImageGalleryItem
-            key={nanoid()}
-            imageData={imageData}
-            openModal={openModal}
-          />
-        ))}
-      </ul>
-    );
-  }
-}
+const ImageGallery = ({ imagesData, openModal }) => {
+  const { gallery } = styles;
+
+  return (
+    <ul className={gallery}>
+      {imagesData.map(imageData => (
+        <ImageGalleryItem
+          key={nanoid()}
+          imageData={imageData}
+          openModal={openModal}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ImageGallery;
 
 ImageGallery.propTypes = {
   imagesData: PropTypes.array.isRequired,
   openModal: PropTypes.func.isRequired,
 };
-
-export default ImageGallery;
