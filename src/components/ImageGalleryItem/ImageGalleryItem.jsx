71fa2b5e14803222d19bf36f60ddab7101d1c377
Anna Ojdana <anna.ojdana@gmail.com>
2022-09-05 19:22:04 +0200

    ImageGalleryItem - code refactor

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,32 +1,27 @@
 import styles from './ImageGalleryItem.module.css';
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class ImageGalleryItem extends Component {
+const ImageGalleryItem = ({ imageData, openModal }) => {
+  const { item, image } = styles;
+  const { webformatURL, tags, largeImageURL, id } = imageData;
+  
+  return (
+    <li className={item}>
+      <img
+        className={image}
+        key={id}
+        src={webformatURL}
+        alt={tags}
+        onClick={() => openModal(largeImageURL, tags)}
+      />
+    </li>
+  );
+};
 
-  render() {
-
-    const { imageData, openModal } = this.props;
-    const { item, image } = styles;
-    const { webformatURL, tags, largeImageURL,id } = imageData;
-
-    return (
-      <li className={item}>
-        <img
-          className={image}
-          key={id}
-          src={webformatURL}
-          alt={tags}
-          onClick={() => openModal(largeImageURL, tags)}
-        />
-      </li>
-    );
-  }
-}
+export default ImageGalleryItem;
 
 ImageGalleryItem.propTypes = {
   imageData: PropTypes.object.isRequired,
   openModal: PropTypes.func.isRequired,
 };
-
-export default ImageGalleryItem;
